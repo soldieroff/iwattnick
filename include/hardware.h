5c143f943ab3acd779d8d4ec31010e1784988eae
zapparello <zapparello@471cdd87-eaa6-6206-c917-50587867c74b>
2014-10-17 10:07:44 +0000

    Upgraded CMSIS files to latest version downloaded from ARM site. Added license stuff.

--- include/hardware.h
+++ include/hardware.h
@@ -6,18 +6,6 @@
 #ifndef __HARDWARE_H__
 #define __HARDWARE_H__
 
-#if defined CORE_CORTEX_M4
-#  define ARM_MATH_CM4
-#elif defined CORE_CORTEX_M3
-#  define ARM_MATH_CM3
-#elif defined CORE_CORTEX_M0PLUS
-#  define ARM_MATH_CM0PLUS
-#elif defined CORE_CORTEX_M0
-#  define ARM_MATH_CM0
-#else
-#  error "Unknown CPU core"
-#endif
-
 // Uncomment to use external crystal
 //#define SYSCLK_FREQ_HSE
 
